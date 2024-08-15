fe5e2b2373886d56a5a104f6dc44f868323253a0
marcantoinem <marc-antoine.m@outlook.com>
2024-08-14 21:55:39 -0400

    Remove useless build

--- public/assets/NotFound-YwLQiNa1.js
+++
@@ -1,1 +0,0 @@
-import{i as t,b as l,P as o,t as r}from"./index-D3FeZMrT.js";var n=r('<div class="flex w-full flex-col items-center justify-center"><h2 class="mb-6 text-3xl font-bold">Page Not Found');const a=()=>(()=>{var e=n();return e.firstChild,t(e,l(o,{to:"/",file:"Home",children:"Return to Home"}),null),e})();export{a as default};
