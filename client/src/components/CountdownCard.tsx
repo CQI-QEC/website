62b90d9a353883940e52d54b37b8c0a416594cb3
marcantoinem <marc-antoine.m@outlook.com>
2024-09-02 20:11:36 -0400

    Improve countdown style

--- client/src/components/CountdownCard.tsx
+++ client/src/components/CountdownCard.tsx
@@ -10,12 +10,12 @@ export default function CountdownCard(props: Props) {
     return (
         <div class="relative perspective">
             <div
-                class="relative rounded-t-lg h-10 w-20 lg:h-20 lg:w-44"
+                class="flex items-center justify-center relative rounded-t-lg h-10 w-20 lg:h-28 lg:w-44"
             >
-                <p class="absolute top-5 lg:top-8 left-0 right-0 text-center text-4xl lg:text-8xl font-bold">
+                <p class="text-center text-4xl lg:text-7xl font-bold z-10">
                     {props.current()}
                 </p>
-                <svg width="100%" height="100%">
+                <svg width="100%" height="100%" class="absolute">
                     <mask id={`${props.id}-m`} fill="#fff">
                         <rect id={`${props.id}-r`} width="100%" height="100%" />
                         <circle r="8" fill="#000" cx="0" cy="100%" />
@@ -26,12 +26,12 @@ export default function CountdownCard(props: Props) {
             </div>
 
             <div
-                class="relative rounded-b-lg h-10 w-20 lg:h-20 lg:w-44"
+                class="flex items-center justify-center relative rounded-t-lg h-10 w-20 lg:h-28 lg:w-44"
             >
-                <p class="absolute bottom-5 lg:bottom-8 left-0 right-0 text-center text-4xl lg:text-8xl font-bold" data-testid={props.id}>
+                <p class="text-center text-4xl lg:text-7xl font-bold z-10" data-testid={props.id}>
                     {props.current()}
                 </p>
-                <svg width="100%" height="100%">
+                <svg width="100%" height="100%" class="absolute">
                     <mask id={`${props.id}-m2`} fill="#fff">
                         <rect id={`${props.id}-r2`} width="100%" height="100%" />
                         <circle r="8" fill="#000" cx="0" cy="0" />
