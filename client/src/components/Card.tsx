4e5d29777f67821e6aa7916262a7b7dd011c23d6
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2024-09-22 19:58:48 -0400

    fix: competitions image are now acceptable

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function Cards(props: Props) {
     return (
-        <div class={"bg-white border border-zinc-200 rounded-lg shadow transition h-[38rem] " + props.theme}>
+        <div class={"bg-white border border-zinc-200 rounded-lg shadow transition" + props.theme}>
             {(props.img != undefined) && <div class="rounded-t-lg w-full h-72 bg-cover bg-bottom"  style={"background-image: url('" + props.img + "')"} ></div>}
             <div class="p-5">
                 {props.children}
