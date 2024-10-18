4eb960d394007225d4b6e28266545cf7a3098885
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2024-10-18 00:02:18 -0400

    feat: prepare offline compilation

--- server/src/routes/get_participants.rs
+++ server/src/routes/get_participants.rs
@@ -1,16 +1,39 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 
-use crate::{auth::claims::Claims, model::preview_participant::ParticipantPreview, SharedState};
+use crate::{
+    auth::claims::Claims,
+    model::{preview_participant::ParticipantPreview, role::Role},
+    SharedState,
+};
 
 pub async fn get_participants(
     claims: Claims,
     State(state): State<SharedState>,
 ) -> impl IntoResponse {
-    match ParticipantPreview::get_participants(&state.db).await {
-        Ok(participants) => (StatusCode::OK, Json(participants)).into_response(),
-        Err(e) => {
-            tracing::error!("Failed to get participants: {}", e);
-            (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response()
+    match claims.role {
+        Role::Organizer | Role::Volunteer => {
+            match ParticipantPreview::get_participants(&state.db).await {
+                Ok(participants) => return (StatusCode::OK, Json(participants)).into_response(),
+                Err(e) => {
+                    tracing::error!("Failed to get participants: {}", e);
+                    return (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response();
+                }
+            }
         }
+        Role::Chef => {
+            match ParticipantPreview::get_participants_from_university(
+                &state.db,
+                &claims.university,
+            )
+            .await
+            {
+                Ok(participants) => return (StatusCode::OK, Json(participants)).into_response(),
+                Err(e) => {
+                    tracing::error!("Failed to get participants: {}", e);
+                    return (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response();
+                }
+            }
+        }
+        Role::Participant => return (StatusCode::FORBIDDEN, "Forbidden").into_response(),
     }
 }
