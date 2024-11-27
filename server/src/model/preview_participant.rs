8875fa9921ffe2d2e433153da0898e9b4b6edf3d
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2024-11-27 12:03:06 -0500

    fix: change checkmark to only take the one with good data p2

--- server/src/model/preview_participant.rs
+++ server/src/model/preview_participant.rs
@@ -21,7 +21,7 @@ pub struct ParticipantPreview {
 impl ParticipantPreview {
     pub async fn get_participants(db: &PgPool) -> Result<Vec<Self>, sqlx::Error> {
         sqlx::query_as::<_, Self>(
-            r#"SELECT id, first_name, last_name, email, role, competition, university, (study_proof IS NOT NULL AND LENGTH(study_proof) = 0) AND (photo IS NOT NULL AND LENGTH(photo) = 0) AND (cv IS NOT NULL AND LENGTH(cv) = 0) as contain_cv FROM participants"#
+            r#"SELECT id, first_name, last_name, email, role, competition, university, (study_proof IS NOT NULL AND LENGTH(study_proof) != 0) AND (photo IS NOT NULL AND LENGTH(photo) != 0) AND (cv IS NOT NULL AND LENGTH(cv) != 0) as contain_cv FROM participants"#
         )
         .fetch_all(db)
         .await
@@ -31,7 +31,7 @@ impl ParticipantPreview {
         db: &PgPool,
         university: University,
     ) -> Result<Vec<Self>, sqlx::Error> {
-        sqlx::query_as("SELECT id, first_name, last_name, email, role, competition, university, (study_proof IS NOT NULL AND LENGTH(study_proof) = 0) AND (photo IS NOT NULL AND LENGTH(photo) = 0) AND (cv IS NOT NULL AND LENGTH(cv) = 0) as contain_cv FROM participants WHERE university = $1 AND (role = 'participant' OR role = 'chef')")
+        sqlx::query_as("SELECT id, first_name, last_name, email, role, competition, university, (study_proof IS NOT NULL AND LENGTH(study_proof) != 0) AND (photo IS NOT NULL AND LENGTH(photo) != 0) AND (cv IS NOT NULL AND LENGTH(cv) != 0) as contain_cv FROM participants WHERE university = $1 AND (role = 'participant' OR role = 'chef')")
             .bind(university)
             .fetch_all(db)
             .await
